759b86c574293852bbdd63fe62f4b0f104cfc4f7
Michael Marino <michael.marino@TIG365.onmicrosoft.com>
2022-01-05 15:50:55 -0500

    Updated prettier config for Windows line endings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-    arrowParens: 'avoid',
-    bracketSpacing: false,
-    singleQuote: true,
-    trailingComma: 'all',
-  };
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
